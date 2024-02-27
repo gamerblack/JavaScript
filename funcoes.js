b1d10dfb728edac3f6daf66010bc32b65f62794b
gaby <125655898+gamerblack@users.noreply.github.com>
2024-02-27 10:39:11 -0300

    Add files via upload

--- funcoes.js
+++ funcoes.js
@@ -6,14 +6,55 @@ console.log(pi);
 let aC = areaCirculo(n);
 console.log(`A área é ${n} cm²`);
 
+console.log(`\n\nFunção área Circulo - Função Declarada por Bloco`);
+
+console.log(`A área do circulo é ${aC}cm²`);
 
 function areaCirculo (raio = 1) {
     console.log(raio);
     return Math.PI * (raio ** 2);
 }
 
+
+console.log(`\n\nFunção área Quadrado - Função Declarada por Expressão`);
+
+const aQ = function (lado){
+    return lado**2;
+}
+
+console.log(`A área é ${aQ(n2)}cm²`);
+
 // TAREFA = calcular a área de um quadrado, e de um triângulo
 // PESQUISAR a Diferença de var, let e const
 
-var l;
-window.alert(`Insira o valor do lado do Quadrado: ${l}`);
+let opcao = window.confirm("Confirme para Calcular a Àrea do Quadrado ou Recuse para seguir ao Calculo da Àrea do Triangulo.");
+
+if  (opcao){
+    function Quadrado () {
+        // Quadrado
+        var l = prompt(`Insira o valor do lado do Quadrado:`);
+        let areaQ = l ** 2;
+        window.alert(`A área do Quadrado é ${areaQ}`);
+    }
+}else {
+    function Triangulo () {
+        // Triângulo
+        var b = prompt(`Insira o valor da Base do Triângulo:`);
+        var h = prompt(`Insira o valor da Altura do Triângulo:`);
+        let areaT = (b * h)/2;
+        window.alert(`A área do Triângulo é ${areaT}`);
+    }
+}
+
+opcao = window.confirm("Deseja Calcular a Área do Triângo?");
+
+if (opcao){
+    // Triângulo
+    var b = prompt(`Insira o valor da Base do Triângulo:`);
+    var h = prompt(`Insira o valor da Altura do Triângulo:`);
+    let areaT = (b * h)/2;
+    window.alert(`A área do Triângulo é ${areaT}`);
+    window.alert(`Tarefa Concluída`);
+}else {
+    window.alert(`Tarefa Concluída`);
+}
